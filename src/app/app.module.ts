6fb93409dcd14e0a456c0ea2888c963bf365eadc
DonnCycu <kglogowski99@gmail.com>
2023-09-14 19:55:30 +0200

    creating a "home" view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found/page-not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { CharactersListComponent } from './characters/characters-list/characters-list.component';
+import { CharactersComponent } from './characters/characters/characters.component';
+import {HttpClientModule} from "@angular/common/http";
+import { CharacterDetailComponent } from './characters/character-detail/character-detail.component';
+
+import { TranslatePipe } from './translate-gender.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    CharactersListComponent,
+    CharactersComponent,
+    CharacterDetailComponent,
+    TranslatePipe
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
